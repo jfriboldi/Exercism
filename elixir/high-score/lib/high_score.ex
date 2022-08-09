7d88e5e00f1adcbd9db378706ee5c2dff3aac1a1
Jorge Riboldi <jfriboldi@gmail.com>
2022-08-09 09:21:08 -0700

    complete high score challenge

---
+++ elixir/high-score/lib/high_score.ex
@@ -0,0 +1,30 @@
+defmodule HighScore do
+  @initial_score 0
+
+  def new() do
+    %{}
+  end
+
+  def add_player(scores, name, score \\ @initial_score) do
+    Map.put(scores, name, score)
+  end
+
+  def remove_player(scores, name) do
+   {_, res} = Map.pop(scores, name)
+    res
+  end
+
+  def reset_score(scores, name) do
+    add_player(scores, name, @initial_score)
+  end
+
+  def update_score(scores, name, score) do
+    Map.update(scores, name, score, fn ev ->
+        ev + score
+      end)
+  end
+
+  def get_players(scores) do
+    Map.keys(scores)
+  end
+end
